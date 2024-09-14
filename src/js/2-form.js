ab2d8b4c4881e40c2eb32c37329e99c61f374176
variabb <barrbarriska1@gmail.com>
2024-09-14 22:28:12 +0300

    gallery

---
+++ src/js/2-form.js
@@ -0,0 +1,29 @@
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+let formData = { email: '', message: '' };
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+if (savedData) {
+  formData = JSON.parse(savedData);
+  form.email.value = formData.email || '';
+  form.message.value = formData.message || '';
+}
+
+form.addEventListener('input', event => {
+  formData[event.target.name] = event.target.value.trim();
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
+  formData = { email: '', message: '' };
+});
